c704ec4224dd7b07f96832532b888df6e372de07
Benoit Leveau <benoit.leveau@gmail.com>
2026-04-02 11:14:14 +0400

    Renamed "Chat Assistant" to "Research Terminal"

--- web/src/components/chat/ChatDrawer.tsx
+++ web/src/components/chat/ChatDrawer.tsx
@@ -500,7 +500,7 @@ export default function ChatDrawer({
               >
                 <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z" />
               </svg>
-              Climate Assistant
+              Research Terminal
             </div>
 
             {/* Dev model selector */}
